651ddde385e189811c2649f3ced03ae4e12bea51
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 12:31:33 +0100

    Create main.rs

---
+++ standard/src/main.rs
@@ -0,0 +1,12 @@
+use rand::Rng;
+
+fn main() {
+    println!("Hello, world!");
+    let uppers      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowers      = "abcdefghijklmnopqrstuvwxyz";
+    let numbers     = "0123456789";
+    let specials    = "!@#£$%&[]+?*-_.:,;";
+
+
+    println!("created by hellmak at GitHub, cyberSecHell at Twitter")
+}
