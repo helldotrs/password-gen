cbf47f2d00f0853365ba765d46e532d99a58a195
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 16:01:35 +0100

    Update prototype.py

--- simple/src/prototype.py
+++ simple/src/prototype.py
@@ -1,14 +1,12 @@
 import random
 
-print(random.randint(3, 9)) 
-
 chars   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 pwl     = 16
 pw      = ""
 
 
 i       = 0
-while (true){
+while (true)
     pw.append(chars[random.randint(0, len(chars))])
     # ^is this clear enough or should I seperate more steps?
     
@@ -16,5 +14,5 @@ while (true){
         break
         
     i += 1
-}
+
 print(pw)
