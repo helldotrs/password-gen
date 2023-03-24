8f20ac35813401ab9f6271398f114345b587bacc
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 16:00:07 +0100

    Update prototype.py

--- simple/src/prototype.py
+++ simple/src/prototype.py
@@ -1,3 +1,7 @@
+import random
+
+print(random.randint(3, 9)) 
+
 chars   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 pwl     = 16
 pw      = ""
@@ -5,10 +9,12 @@ pw      = ""
 
 i       = 0
 while (true){
-    
+    pw.append(chars[random.randint(0, len(chars))])
+    # ^is this clear enough or should I seperate more steps?
     
     if (i >= pwl):
         break
         
     i += 1
 }
+print(pw)
