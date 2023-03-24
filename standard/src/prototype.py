84525f69ef45105e4ea43f4cd2a3847ef5bd1ec1
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 16:11:01 +0100

    Create prototype.py

---
+++ standard/src/prototype.py
@@ -0,0 +1,18 @@
+import random
+
+chars   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#£$%&[]+?*-_.:,;"
+pwl     = 16
+pw      = ""
+
+
+i       = 0
+while (True):
+    pw += chars[random.randint(0, len(chars))-1]
+    # ^is this clear enough or should I seperate more steps?
+    
+    if (i >= pwl):
+        break
+        
+    i += 1
+
+print(pw)
