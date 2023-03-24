6b550f320f424cb02e4254f436450581fafc5220
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 12:40:20 +0100

    Update main.rs

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -2,11 +2,10 @@ use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
-    let uppers      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowers      = "abcdefghijklmnopqrstuvwxyz";
-    let numbers     = "0123456789";
-    let specials    = "!@#£$%&[]+?*-_.:,;";
+    let characters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let chars_len   = characters.len();
+    
 
-
-    println!("created by hellmak at GitHub, cyberSecHell at Twitter")
+    println!("created by hellmak at GitHub, cyberSecHell at Twitter");
+    println!("{}", chars_len);
 }
