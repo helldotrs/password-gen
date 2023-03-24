8087c55f38b06c8dfd46207a1399c7c37551c1c7
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-03-24 16:25:18 +0100

    Update main.rs

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -2,9 +2,16 @@ use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
-    let characters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let chars_len   = characters.len();
-    
+    let     characters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let     chars_len   = characters.len();
+    let     passw_len   = 16;
+
+    let mut password    = String::new();
+    let     rand_max    = rand::thread_rng().gen_range(0, chars_len;   
+
+    loop{
+        password += chars
+    }
 
     println!("created by hellmak at GitHub, cyberSecHell at Twitter");
     println!("{}", chars_len);
